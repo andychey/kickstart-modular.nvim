1c0b657674b5cebc56073cec0d1b7076fdd33f93
Andy Chey <andychey@outlook.com>
2025-08-16 08:34:25 +0800

    feat: add the custom plugin hop.vim

---
+++ lua/custom/plugins/hop.nvim.lua
@@ -0,0 +1,46 @@
+return {
+  'smoka7/hop.nvim',
+  version = '*',
+  config = function()
+    local hop = require 'hop'
+    local hint = require 'hop.hint'
+    local directions = hint.HintDirection
+    local positions = hint.HintPosition
+
+    hop.setup {}
+
+    vim.keymap.set('n', 'f', function()
+      hop.hint_char1 { direction = directions.AFTER_CURSOR }
+    end, { remap = true })
+    vim.keymap.set('n', 'F', function()
+      hop.hint_char1 { direction = directions.BEFORE_CURSOR }
+    end, { remap = true })
+    vim.keymap.set('n', 't', function()
+      hop.hint_char1 { direction = directions.AFTER_CURSOR, hint_offset = -1 }
+    end, { remap = true })
+    vim.keymap.set('n', 'T', function()
+      hop.hint_char1 { direction = directions.BEFORE_CURSOR, hint_offset = 1 }
+    end, { remap = true })
+    vim.keymap.set('n', '<leader>x', function()
+      hop.hint_char2 {}
+    end, { remap = true })
+    vim.keymap.set('n', '<leader>w', function()
+      hop.hint_words { direction = directions.AFTER_CURSOR }
+    end, { remap = true })
+    vim.keymap.set('n', '<leader>b', function()
+      hop.hint_words { direction = directions.BEFORE_CURSOR }
+    end, { remap = true })
+    vim.keymap.set('n', '<leader>e', function()
+      hop.hint_words { direction = directions.AFTER_CURSOR, hint_position = positions.END }
+    end, { remap = true })
+    vim.keymap.set('n', '<leader>ge', function()
+      hop.hint_words { direction = directions.BEFORE_CURSOR, hint_position = positions.END }
+    end, { remap = true })
+    vim.keymap.set('n', '<leader>j', function()
+      hop.hint_lines_skip_whitespace { direction = directions.AFTER_CURSOR }
+    end, { remap = true })
+    vim.keymap.set('n', '<leader>k', function()
+      hop.hint_lines_skip_whitespace { direction = directions.BEFORE_CURSOR }
+    end, { remap = true })
+  end,
+}
