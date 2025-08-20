8f0f4e11ad2bddc43c366026b888012345d3607a
Andy Chey <andychey@outlook.com>
2025-08-20 15:20:15 +0800

    feat: hop.nvim

--- lua/custom/plugins/hop.nvim.lua
+++ lua/custom/plugins/hop.nvim.lua
@@ -9,37 +9,43 @@ return {
 
     hop.setup {}
 
-    vim.keymap.set('n', 'f', function()
+    --[[
+    vim.keymap.set({ 'n' }, 'f', function()
       hop.hint_char1 { direction = directions.AFTER_CURSOR }
     end, { remap = true })
-    vim.keymap.set('n', 'F', function()
+    vim.keymap.set({ 'n' }, 'F', function()
       hop.hint_char1 { direction = directions.BEFORE_CURSOR }
     end, { remap = true })
-    vim.keymap.set('n', 't', function()
+    vim.keymap.set({ 'n' }, 't', function()
       hop.hint_char1 { direction = directions.AFTER_CURSOR, hint_offset = -1 }
     end, { remap = true })
-    vim.keymap.set('n', 'T', function()
+    vim.keymap.set({ 'n' }, 'T', function()
       hop.hint_char1 { direction = directions.BEFORE_CURSOR, hint_offset = 1 }
     end, { remap = true })
-    vim.keymap.set('n', '<leader>x', function()
+		]]
+
+    vim.keymap.set({ 'o' }, 's', function()
       hop.hint_char2 {}
     end, { remap = true })
-    vim.keymap.set('n', '<leader>w', function()
+    vim.keymap.set({ 'n', 'v' }, '<leader>x', function()
+      hop.hint_char2 {}
+    end, { remap = true })
+    vim.keymap.set({ 'n', 'v' }, '<leader>w', function()
       hop.hint_words { direction = directions.AFTER_CURSOR }
     end, { remap = true })
-    vim.keymap.set('n', '<leader>b', function()
+    vim.keymap.set({ 'n', 'v' }, '<leader>b', function()
       hop.hint_words { direction = directions.BEFORE_CURSOR }
     end, { remap = true })
-    vim.keymap.set('n', '<leader>e', function()
+    vim.keymap.set({ 'n', 'v' }, '<leader>e', function()
       hop.hint_words { direction = directions.AFTER_CURSOR, hint_position = positions.END }
     end, { remap = true })
-    vim.keymap.set('n', '<leader>ge', function()
+    vim.keymap.set({ 'n', 'v' }, '<leader>ge', function()
       hop.hint_words { direction = directions.BEFORE_CURSOR, hint_position = positions.END }
     end, { remap = true })
-    vim.keymap.set('n', '<leader>j', function()
+    vim.keymap.set({ 'n', 'v' }, '<leader>j', function()
       hop.hint_lines_skip_whitespace { direction = directions.AFTER_CURSOR }
     end, { remap = true })
-    vim.keymap.set('n', '<leader>k', function()
+    vim.keymap.set({ 'n', 'v' }, '<leader>k', function()
       hop.hint_lines_skip_whitespace { direction = directions.BEFORE_CURSOR }
     end, { remap = true })
   end,
